e7f1e28cfe1ec74f0f45bcc41c84a38b7e49692e
Solon Thiago <stgmoreira2906@gmail.com>
2025-10-29 12:27:38 -0300

    adição de containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
-import { TimerIcon } from 'lucide-react';
+import { Container } from './components/Container';
 import { Heading } from './components/Heading';
-
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Meu App
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur quo consequuntur, ea magnam dolorum ex quas
-        iure tempora corporis, a eos harum at eius aliquid, dignissimos inventore quod culpa nostrum.
-      </p>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
     </>
   );
 }
