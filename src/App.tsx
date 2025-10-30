33bff547c32ad6ddec90867c94b401f3042e0885
Solon Thiago <stgmoreira2906@gmail.com>
2025-10-30 13:06:21 -0300

    Adição de novos elementos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Container } from './components/Container';
 import { CountDown } from './components/CountDown';
+import { Cycles } from './components/Cycles';
+import { DefaultInput } from './components/Defaultinput';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 
@@ -19,6 +21,26 @@ export function App() {
 
       <Container>
         <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput labelText='Tarefa' id='meuInput' type='text' placeholder='Digite aqui' />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <Cycles />
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
